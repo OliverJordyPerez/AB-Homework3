44c164efd65bdb5bd409a4707494bd316dc38065
Oliver Jordy Pérez Escamilla <oliverjordyperezescamilla@MacBook-Pro-de-Oliver.local>
2020-06-15 08:04:05 -0500

    [Feature] Show quotes when pressing button
    - [X] Add random quotes when pressing New quote button
    - [X] Handle orientation changes to persist quote

---
+++ PersonalCard/app/src/main/java/com/oliverperez/personalcard/DataSource.kt
@@ -0,0 +1,27 @@
+package com.oliverperez.personalcard
+
+final class DataSource {
+
+    var currentQuote: String = ""
+
+    private var quotes = mutableListOf<String>(
+        "“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” ${'\n'}  ${'\t'} – Martin Fowler",
+        "“First, solve the problem. Then, write the code.” ${'\n'}  ${'\t'} – John Johnson",
+        "“ Code is like humor. When you have to explain it, it’s bad.” ${'\n'}  ${'\t'} – Cory House",
+        "“Before software can be reusable it first has to be usable.” ${'\n'}  ${'\t'} – Ralph Johnson",
+        "“Make it work, make it right, make it fast.” ${'\n'}  ${'\t'} – Kent Beck"
+    )
+
+    fun getNewQuote(): String {
+        var newQuote = quotes.random()
+        return if (quotes.count() > 1) {
+            while (newQuote == currentQuote) {
+                newQuote = quotes.random()
+            }
+            currentQuote = newQuote
+            newQuote
+        } else {
+            newQuote
+        }
+    }
+}
